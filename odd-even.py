cb63893d1f2536b04ebd82bafb81f93c81932b82
Aryan Anand <anandaryan406@gmail.com>
2026-02-08 03:22:27 +0530

    Add files via upload

---
+++ odd-even.py
@@ -0,0 +1,6 @@
+num=int(input("Enter any number to find it is even or odd:"))
+
+if num % 2 == 0:
+    print(f"num:{num} is an even number ")
+else:
+    print(f"num:{num} is an odd number ")
---
+++ sum.py
@@ -0,0 +1,7 @@
+sum_total=0
+
+for i in range(1,51):
+    sum_total += i
+    
+#Output of the sum of number from 1-50 
+print(f"Sum_total: {sum_total} the total sum of number from 1-50 is:",sum_total)
